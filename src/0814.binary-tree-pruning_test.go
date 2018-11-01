c342d1e55d6bf2a74d305730b2b44d4c42c72741
caoxiaolin <xmihu945@qq.com>
2018-11-01 17:48:53 +0800

    modify by caoxiaolin

--- src/0814.binary-tree-pruning_test.go
+++ src/0814.binary-tree-pruning_test.go
@@ -6,11 +6,15 @@ import (
 )
 
 func TestPruneTree(t *testing.T) {
-	s := "1000101"
-	treenode := PreorderBuildBT(s)
-	tree := pruneTree(treenode)
+	list := make([]interface{}, 5)
+	list[0] = 1
+	list[1] = nil
+	list[2] = 0
+	list[3] = 0
+	list[4] = 1
+	tree := LevelorderBuildBT(list)
 
-	result := PreorderBT(tree)
+	result := pruneTree(tree)
 
-	fmt.Printf("%v\n", result)
+	fmt.Printf("%v\n", LevelorderBT(result))
 }
